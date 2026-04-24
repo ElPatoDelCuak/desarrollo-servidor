d8c410651065128672c6f863d83139d74d14dece
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-04-24 16:15:50 +0200

    aaa

--- RA4/locallibrary/catalog/views.py
+++ RA4/locallibrary/catalog/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
-
 def index(request):
     """View function for home page of site."""
 
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
 
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
-    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     context = {
@@ -21,7 +16,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'authors': Author.objects.all(),
+        'books': Book.objects.all(),
     }
 
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'catalog/index.html', context=context)
